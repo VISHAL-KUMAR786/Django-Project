1e67c6bae6378d0796c3c91942313e6c1e722584
VISHAL-KUMAR786 <vishalkumar18ssb786@gmail.com>
2022-05-03 22:50:34 +0530

    making topic , message , room models

--- Discord/base/models.py
+++ Discord/base/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Topic(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self) -> str:
+        return self.name[:50]
+    pass 
+
+class Room(models.Model):
+    host = models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
+    topic = models.ForeignKey(Topic,on_delete=models.SET_NULL,null=True) # if topic was not above room then 'Topic'
+    name = models.CharField(max_length=200)
+    description = models.TextField(max_length=2000,null=True,blank=True)
+    # participants = models. 
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+    pass 
+
+class Message(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE) #(who is sending message)
+    room = models.ForeignKey(Room,on_delete=models.CASCADE)
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.body[:50]
+    pass 
--- Discord/base/views.py
+++ Discord/base/views.py
@@ -1,18 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
-rooms = [
-    {'id': 1, 'name': 'Lets Learn Python!'},
-    {'id': 2, 'name': 'Design with me'},
-    {'id': 3, 'name': 'frontend developers'},
-]
+from .models import Room
 
 
 def index(request):
-    content = {'rooms':rooms}
-    return render(request, 'base/home.html',content)
+    rooms = Room.objects.all()
+    content = {'rooms': rooms}
+    return render(request, 'base/home.html', content)
 
 
-def room(request,pk):
-    content = {'pk':pk}
-    return render(request, 'base/room.html',content)
+def room(request, pk):
+    room = None
+    # for i in rooms:
+    #     if i['id'] == int(pk):
+    #         room = i
+    room = Room.objects.get(id=pk)
+    content = {'room': room}
+    return render(request, 'base/room.html', content)
