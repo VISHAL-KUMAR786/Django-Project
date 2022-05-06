de80b89ec282b1685fbeec7affadf4c9913b3c9b
VISHAL-KUMAR786 <vishalkumar18ssb786@gmail.com>
2022-05-06 20:54:19 +0530

    small backup for ui setup

--- Discord/Discord/settings.py
+++ Discord/Discord/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -121,7 +122,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,"static"),
+]
+
+MEDIA_URL = 'media/' # this is when the data is fetch
+MEDIA_ROOT = os.path.join(BASE_DIR , 'static/media') # where to store file
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
